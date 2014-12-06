3b3c0ae4a736a823e612eea246432fbcfeea961c
Gonzalo J. García Martín <alu0100403619@ull.edu.es>
2014-12-06 19:59:38 +0000

    intento de mostrar variables de la clase de java

--- Tutoriales/Tutorial_1/Subtutorial_4/app/src/main/java/com/example/gonzalo/lifecycle/Jugar.java
+++ Tutoriales/Tutorial_1/Subtutorial_4/app/src/main/java/com/example/gonzalo/lifecycle/Jugar.java
@@ -2,15 +2,54 @@ package com.example.gonzalo.lifecycle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 public class Jugar extends Activity {
 
-	/** Called when the activity is first created. */
+    static final String STATE_SCORE = "playerScore";
+    static final String STATE_LEVEL = "playerLevel";
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int score = 50;
+    public int level = 7;
+
+    /** Called when the activity is first created. */
 	@Override
   public void onCreate (Bundle savedInstanceState) {
+      //Toast.makeText(this, "Level: " + getLevel(), Toast.LENGTH_LONG);
+      //Toast.makeText(this, "Score: " + getScore(), Toast.LENGTH_LONG);
 	  super.onCreate (savedInstanceState);
 	  setContentView (R.layout.jugar);
 	}
+
+  public void onSaveInstanceState (Bundle savedInstanceState) {
+      setScore(getScore() + getLevel());
+      setLevel(getLevel() + 1);
+      savedInstanceState.putInt(STATE_SCORE, getScore());
+      savedInstanceState.putInt(STATE_LEVEL, getLevel());
+      super.onSaveInstanceState(savedInstanceState);
+  }
+
+  public void onRestoreInstanceState(Bundle savedInstanceState) {
+      super.onRestoreInstanceState(savedInstanceState);
+      score = savedInstanceState.getInt(STATE_SCORE);
+      level = savedInstanceState.getInt(STATE_LEVEL);
+  }
 
 }
