c5927778e1b45fbe231784482aa2161ccbe2dedc
Gonzalo J. García Martín <alu0100403619@ull.edu.es>
2014-12-07 17:15:46 +0000

    Tutorial 1 - subtutorial 5 Fragmentos

---
+++ Tutoriales/Tutorial_1/Subtutorial_5/app/src/main/java/com/example/gonzalo/fragments/Ipsum.java
@@ -0,0 +1,14 @@
+package com.example.gonzalo.fragments;
+
+public class Ipsum {
+
+    static String[] Headlines = {
+        "Artículo Uno",
+        "Artículo Dos"
+    };
+
+    static String[] Articles = {
+        "Artículo Uno\n\nEsto es un artículo de prueba, el articulo uno",
+        "Artículo Dos\n\nAquí tenemos otro artículo, el numero dos."
+    };
+}
