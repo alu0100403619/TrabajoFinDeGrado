32d5bace94741499402b66ecbe833b5003733a04
Gonzalo J. García Martín <alu0100403619@ull.edu.es>
2014-12-09 18:04:37 +0000

    Tutrial 2 - Subtutorial 3 - Compartiendo Archivos con NFC

---
+++ Tutoriales/Tutorial_2/Subtutorial_3/app/src/main/java/com/example/gonzalo/sharingfilesnfc/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.gonzalo.sharingfilesnfc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.nfc.NfcAdapter;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+
+    private NfcAdapter nfcAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PackageManager pm = this.getPackageManager();
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_NFC)) {
+            Toast.makeText(this, "The device does not has NFC hardware.",
+                    Toast.LENGTH_SHORT).show();
+        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            Toast.makeText(this, "Android Beam is not supported.",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Android Beam is supported on your device.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendByNFC (View view) {
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (!nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "Please enable NFC.",
+                    Toast.LENGTH_SHORT).show();
+            startActivity (new Intent (Settings.ACTION_NFC_SETTINGS));
+        } else if (!nfcAdapter.isNdefPushEnabled()) {
+            Toast.makeText(this, "Please enable Android Beam.",
+                    Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
+        } else {
+            String fileName = "jacksally.jpeg";
+            File fileDirectory = Environment
+                    .getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_PICTURES);
+            File fileToTransfer = new File(fileDirectory, fileName);
+            fileToTransfer.setReadable(true, false);
+            nfcAdapter.setBeamPushUris(
+                    new Uri[] {Uri.fromFile(fileToTransfer)}, this);
+        }
+    }
+}
