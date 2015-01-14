6019f2c873fc70be050e2f9492c23d3648409a18
Gonzalo J. García Martín <alu0100403619@ull.edu.es>
2015-01-14 16:02:59 +0000

    Tutorial 5 - Subtutorial 6 Transferring Data Using Sync Adapters (No se Inicia la Aplicaión)

---
+++ Tutoriales/Tutorial_5/Subtutorial_6/app/src/main/java/com/example/gonzalo/feedsyncsample/EntryListActivity.java
@@ -0,0 +1,16 @@
+package com.example.gonzalo.feedsyncsample;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Gonzalo on 14/01/2015.
+ */
+public class EntryListActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_list);
+    }
+}
